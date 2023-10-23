472a8339120bf9b8f9736fdf1bd687e83a88ead7
Jixian-Jia-Lax <105738511+Jixian-Jia@users.noreply.github.com>
2023-10-23 02:19:05 -0700

    first part hm finished

--- run.py
+++ run.py
@@ -8,30 +8,30 @@ def experiment(num_of_pages, num_of_trials):
 
     # Convert the captured output to a long int
     print("DEBUG: result.stdout:", result.stdout)
-    result = int(result.stdout.strip())
+    result = float(result.stdout.strip())
 
-    return result
+    return (result*pow(10,3))/num_of_trials
 
 def main():
-    num_of_trials = 10
+    num_of_trials = 5000
     num_of_pages = 2
     iterations = 10
 
-    param1_values = []
+    num_of_pages_values = []
     c_program_results = []
 
     for _ in range(iterations):
         result = experiment(num_of_pages, num_of_trials)
         
-        param1_values.append(num_of_pages)
+        num_of_pages_values.append(num_of_pages)
         c_program_results.append(result)
 
         num_of_pages *= 2  # double the value of param1 in each iteration
 
     # Plotting using matplotlib
-    plt.plot(param1_values, c_program_results, 'o-', label='Results from C program')
+    plt.plot(num_of_pages_values, c_program_results, 'o-', label='Results from C program')
     plt.xlabel('Number of Pages')
-    plt.ylabel('Time Per Access')
+    plt.ylabel('Time Per Access (ns)')
     plt.title('Param1 vs Results from C program')
     plt.legend()
     plt.grid(True)
--- tlb.c
+++ tlb.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <unistd.h>
-
+#include <mach/mach.h>
+#include <mach/thread_policy.h>
+#include <mach/thread_act.h>
 
 int main(int argc, char* argv[]){
+
+    thread_affinity_policy_data_t policy = {1};
+    mach_port_t thread = mach_thread_self();
+
+    kern_return_t result = thread_policy_set(thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
     int num_pages = atoi(argv[1]);
     int num_trials = atoi(argv[2]);
     int pagesize = (int)sysconf(_SC_PAGESIZE);
@@ -15,6 +22,10 @@ int main(int argc, char* argv[]){
     struct timeval start, end;
     int j;
     int i;
+
+    for(i = 0; i< num_pages*jump; i+= jump){
+            a[i] += 1;
+        }
     for(j = 0; j<num_trials; j++){
         
         gettimeofday(&start, NULL);
@@ -26,6 +37,6 @@ int main(int argc, char* argv[]){
     }
 
     free(a);
-   
-    return counter;
+    printf("%lf\n", ((double)counter)/num_pages); 
+    return 0;
 }
